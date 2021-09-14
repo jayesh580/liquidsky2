791ebefba8398d21011b11da8bcfa44d2eb5aa5c
jayesh580 <71544219+jayesh580@users.noreply.github.com>
2021-09-14 18:05:39 +0530

    Add files via upload

---
+++ js/water-ripple/main2.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function(){
+
+	"use strict"
+
+        $('#ripp').ripples({
+            dropRadius: 10,
+            perturbance: 0.01,
+        });
+        $('#ripp1').ripples({
+            dropRadius: 10,
+            perturbance: 0.01,
+        });
+    
+    
+    });
